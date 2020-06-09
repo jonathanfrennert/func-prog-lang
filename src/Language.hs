3f23829674ee6545c3bcadfe45112485d1a98777
Jonathan Gustafsson Frennert <jonathan.frennert@gmail.com>
2020-06-09 09:11:06 +0200

    Added final language expressions (CH 1.3)

--- src/Language.hs
+++ src/Language.hs
@@ -2,7 +2,7 @@ module Language where
 
 data Expr a
   = EVar Name                 -- ^ Variables
-  | Enum Int                  -- ^ Numbers
+  | ENum Int                  -- ^ Numbers
   | EConstr Int Int           -- ^ Constructor tag arity
   | EAp (Expr a) (Expr a)     -- ^ Application
   | ELet                      -- ^ Let(rec) expressions
@@ -16,20 +16,32 @@ data Expr a
   deriving(Show)
 
 type Name = String
+
 type CoreExpr = Expr Name
 
-type CoreAlt = Alter Name
 type Alter a = (Int, [a], Expr a)
+type CoreAlt = Alter Name
 
 type IsRec = Bool
 recursive, nonRecursive :: IsRec
 recursive = True
 nonRecursive = False
 
--- * Helper methods
+type Program a = [ScDefn a]
+type CoreProgram = Program Name
+
+type ScDefn a = (Name, [a], Expr a)
+type CoreScDefn = ScDefn Name
+
+-- | Helper methods
 
 bindersOf :: [(a,b)] -> [a]
 bindersOf defns = [name | (name, rhs) <- defns]
 
 rhssOf :: [(a,b)] -> [b]
 rhssOf defns = [rhs | (name, rhs) <- defns]
+
+isAtomicExpr :: Expr a -> Bool
+isAtomicExpr (EVar v) = True
+isAtomicExpr (ENum n) = True
+isAtomicExpr e = False
